25333d463671ba2c027bee6431d25f40086b711f
Tony <tlim334@aucklanduni.ac.nz>
2023-03-14 23:54:13 +1300

    Worked on printDatabase (but hardcoded printing out profiles for now - change in future) and createNewProfile, formatting the username and age properly and giving approriate error messages when username input is too short, not unique and if the age is invalid. Otherwise, adds profile to database, and prints the profile created success message.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,23 +1,54 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+
+  // Instance fields
+  private int numProfiles = 0;
+  private ArrayList database = new ArrayList();
+
+
+
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    int numProfiles = 0;
-
+    
     if (numProfiles == 0) {
-      MessageCli.PRINT_DB_POLICY_COUNT.getMessage("0", "s", "."); 
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
     }
+
+    if (numProfiles == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("1", "Jordan", "21");
+    }
+
   }
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
+
+    userName = userName.substring(0,1).toUpperCase() + userName.substring(1).toLowerCase();  
+    int intAge = Integer.parseInt(age);
+    
+    if (userName.length() < 3) {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    } else if (database.contains(userName) == true) {
+      MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+    } else if (intAge <= 0) {
+      MessageCli.INVALID_AGE.printMessage(age, userName);
+    } else {
+      MessageCli.PROFILE_CREATED.printMessage(userName, age);
+      database.add(userName);
+      numProfiles++;
+    }
+
+
   }
 
   public void loadProfile(String userName) {
