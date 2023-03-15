7746911bc5559d5393a0009180eb1056528a1074
Tony <tlim334@aucklanduni.ac.nz>
2023-03-15 17:40:09 +1300

    Solution for Checkpoint 1 passes all viewable test cases (printDatabase & createNewProfile), however, does not utilise Profiles class (relies on local ArrayLists). Fix in future and add own test cases

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -7,31 +7,30 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class InsuranceSystem {
 
   // Instance fields
-  private ArrayList<Profile> database = new ArrayList<Profile>();
-
+  // ArrayLists of both userNames and ages (indexes of each userName in userNameList corresponds to the same index of age in ageList)
+  private ArrayList<String> userNameList = new ArrayList<String>();
+  private ArrayList<String> ageList = new ArrayList<String>();
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    int numProfiles = database.size();
-    String numProfilesString = Integer.toString(numProfiles);
-    
-    if (database.size() == 0) {
+
+    if (userNameList.size() == 0) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
     }
 
-    if (database.size() == 1) {
+    if (userNameList.size() == 1) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
-      // FIX: very unreadable code to extract username and age from database to print out
-      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(numProfilesString, database.get(0).getUsername(), Integer.toString (database.get(0).getAge()));
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("1", userNameList.get(0), ageList.get(0));
     }
 
-    if (database.size() > 1) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(numProfilesString, "s", ":");
-
-      // implement logic to print whole database as stored in ArrayList using for / for each loop
+    if (userNameList.size() > 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(userNameList.size()), "s", ":");
+      for (int i = 0; i < userNameList.size(); i++) {
+        MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(i + 1), userNameList.get(i), ageList.get(i));
+      }
     }
   }
 
@@ -42,20 +41,18 @@ public class InsuranceSystem {
     int intAge = Integer.parseInt(age);
 
     Profile newProfile = new Profile(userName, intAge);
-    
-    // This logic prints out username extracted from the database
-    // System.out.println(database.get(0).getUsername());
-    
+       
     if (userName.length() < 3) {
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
       // FIX THIS boolean statement
-    } else if (userName != newProfile.getUsername()) {
+    } else if (userNameList.contains(userName)) {
       MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
     } else if (intAge <= 0) {
       MessageCli.INVALID_AGE.printMessage(age, userName);
     } else {
       MessageCli.PROFILE_CREATED.printMessage(userName, age);
-      database.add(newProfile);
+      userNameList.add(userName);
+      ageList.add(age);
     }
   }
 
